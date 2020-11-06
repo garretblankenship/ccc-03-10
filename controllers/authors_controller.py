701b34f61b950f77dfecb1eb2e2cbdd8a32cf8e6
garretblankenship <blankenship.151@gmail.com>
2020-11-06 14:38:20 +1100

    setup sqlalchemy and the book model

--- controllers/authors_controller.py
+++
@@ -1,8 +0,0 @@
-from database import cursor, connection
-from flask import Blueprint, request, jsonify
-authors = Blueprint("authors", __name__, url_prefix="/authors")
-
-@authors.route("/", methods=["GET"])
-def author_index():
-    #Return all books
-    return "all authors"
--- database.py
+++ database.py
@@ -1,15 +1,25 @@
-import psycopg2
+from flask_sqlalchemy import SQLAlchemy
 import os
 
-connection = psycopg2.connect(
-    database="library_api",
-    user="postgres",
-    password=os.getenv("DB_PASSWORD"),
-    host="54.252.236.254",
-    port="5432"
-)
+def init_db(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql+psycopg2://postgres:{os.getenv('DB_PASSWORD')}@54.252.236.254:5432/library_api"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db = SQLAlchemy(app)
+    return db
 
-cursor = connection.cursor()
 
-cursor.execute("create table if not exists books (id serial PRIMARY KEY, title varchar);")
-connection.commit()
+# import psycopg2
+# import os
+
+# connection = psycopg2.connect(
+#     database="library_api",
+#     user="postgres",
+#     password=os.getenv("DB_PASSWORD"),
+#     host="54.252.236.254",
+#     port="5432"
+# )
+
+# cursor = connection.cursor()
+
+# cursor.execute("create table if not exists books (id serial PRIMARY KEY, title varchar);")
+# connection.commit()
